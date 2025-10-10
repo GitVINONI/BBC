1b6821d18514f52d0001e4a3dea1d4fa946d75f6
Viktor <vinoni2007@mail.ru>
2025-10-11 01:51:37 +0300

    Third level complete

---
+++ homework_1/level3.py
@@ -0,0 +1,16 @@
+a, znak, b = map(str, input().split())
+
+a = int(a)
+b = int(b)
+
+def arifm(a, b, znak):
+    if znak == "+" :
+        print(f"{a} + {b} = {a + b}")
+    elif znak == "-":
+        print(f"{a} - {b} = {a - b}")
+    elif znak == "*":
+        print(f"{a} * {b} = {a * b}")
+    else:
+        print(f"{a} / {b} = {a / b}")
+
+arifm(a, b, znak)
